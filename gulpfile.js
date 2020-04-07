fb1157396294472b14a6f56ad12168e36d983e44
John <kostia.ledenev@yandex.ru>
2020-04-07 20:13:56 +0400

    double commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
+var svgSprite = require('gulp-svg-sprite');
 const rename = require('gulp-rename');
 const del = require('del');
 const imagemin = require('gulp-imagemin');
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const browserSync = require('browser-sync').create();
 
 const paths = {
     root: './dist',
@@ -17,7 +21,33 @@ const paths = {
         main: './src/assets/styles/main.scss',
         src: './src/assets/styles/**/*.scss',
         dest: './dist/assets/styles'
+    },
+    fonts: {
+        src: './src/assets/styles/fonts.scss',
+        dest: './dist/assets/fonts/'
+    },
+    images: {
+        src: './src/assets/images/*.*',
+        dest: './dist/assets/images/'
+    },
+    scripts: {
+        src: './src/assets/scripts/*.js',
+        dest: './dist/assets/scripts/'
     }
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.fonts.src, fonts);
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
 }
 
 function imgCompress() {
@@ -31,6 +61,20 @@ function imgCompress() {
 function clean() {
     return del(paths.root);
 }
+
+//svg-sprite
+gulp.task('svgSprite', function() {
+    return gulp.src('src/assets/images/icons/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        }
+    ))
+    .pipe(gulp.dest('dist/assets/images/'));
+})
 
 function templates() {
     return gulp.src(paths.templates.pages)
@@ -48,7 +92,34 @@ function styles() {
         .pipe(gulp.dest(paths.styles.dest))
 }
 
+//fonts
+function fonts() {
+    return gulp.src('src/assets/fonts/*.{otf,ttf,woff,woff2}')
+        .pipe(gulp.dest('dist/assets/fonts/'))
+}
+
+function images() {
+    return gulp.src('src/assets/images/*.*')
+        .pipe(gulp.dest('dist/assets/images/'))
+}
+
+//webpack
+function scripts() {
+    return gulp.src([paths.scripts.src])
+        .pipe(gulpWebpack(webpackConfig, webpack))
+        .pipe(gulp.dest(paths.scripts.dest))
+}
+
 exports.templates = templates;
 exports.styles = styles;
+exports.fonts = fonts;
+exports.images = images;
+exports.scripts = scripts;
 exports.clean = clean;
 exports.imgCompress = imgCompress;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, fonts, images),
+    gulp.parallel(watch, server)
+));
